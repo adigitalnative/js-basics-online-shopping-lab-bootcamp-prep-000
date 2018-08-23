edfa8c6b8a314776b155c5524c08b1d2aaf766d2
adigitalnative@gmail.com <adigitalnative@gmail.com>
2018-08-23 19:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,9 +62,14 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  const totalCost = total();
-  cart = []
-  return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
+  if (cardNumber) {
+    const totalCost = total();
+    cart = []
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
+  } else {
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  
 }
 
 
