0d491073f5dfe89d82acb96faf2cd6f72c73eb80
adigitalnative@gmail.com <adigitalnative@gmail.com>
2018-08-23 17:23:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ function viewCart() {
   if (getCart().length === 0) {
     return "Your shopping cart is empty."
   } else if (getCart().length === 1) {
-    return getCart()[0];
-    // return `In your cart, you have foo at $foo.`
+    return `In your cart, you have ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
   }
 }
 
